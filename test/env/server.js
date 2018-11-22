48c4ccd30c2f92d74cdecaf098680140e447c08c
李雨田 <demon@danwi.me>
2018-11-22 15:02:04 +0800

    关闭测试的运行时错误输出

--- test/env/server.js
+++ test/env/server.js
@@ -2,7 +2,7 @@ import {Kato} from "../..";
 import {Misc} from "./api/misc";
 
 export async function startServer() {
-  let kato = new Kato();
+  let kato = new Kato({outputRuntimeError: false});
   kato.load(Misc);
   await kato.listen(0);
   kato.server.prefix = "";

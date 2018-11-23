1cc8679a0e0e7eae1cd12d7fccee95618301340d
李雨田 <demon@danwi.me>
2018-11-23 23:17:09 +0800

    关闭压缩,防止出现class,function,parameter名字被压缩的情况

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,9 @@ const base = require('./webpack.base');
 
 module.exports = merge(base, {
   mode: 'production',
+  optimization: {
+    minimize: false
+  },
   entry: {
     index: './index.ts',
   }

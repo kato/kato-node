603c111387ffd0ef4f1a9eed167b6f25836a50d8
李雨田 <demon@danwi.me>
2018-11-24 21:42:23 +0800

    根据and和or组合函数的逻辑特殊性,加速验证函数的返回

--- core/middleware/auth.ts
+++ core/middleware/auth.ts
@@ -31,55 +31,84 @@ export function auth(...authFuncs: AuthFunction[]) {
   }
 }
 
+//产生一个新的验证函数,新验证函数的返回值与原验证函数相反
 export function not(authFunc: AuthFunction): AuthFunction {
   return async function (ctx: Context) {
     if (typeof authFunc === 'function')
       return !(await authFunc(ctx));
     else
-      return false;
+      return true;
   }
 }
 
+//产生一个新的验证函数,将原验证函数返回值做or运算
 export function or(...authFuncs: AuthFunction[]): AuthFunction {
-  return async function (ctx: Context) {
-    //并行地执行验证函数
-    const promises = authFuncs.map(func => {
-      const promise = func(ctx) as any;
-      if (promise.then) {
-        return promise;
-      } else {
-        //如果不是一个promise,将结果转换为一个promise
-        return Promise.resolve(promise)
+  return function (ctx: Context) {
+    if (authFuncs.length < 1)
+      return true;
+
+    return new Promise((resolve, reject) => {
+      //并行地执行验证函数,并拿到结果
+      const results = authFuncs.map(func => {
+        const result = func(ctx) as any;
+        //将返回值promise化
+        return result.then ? result : Promise.resolve(result);
+      });
+
+      //已经完成的promise计数
+      let completed = 0;
+      //轮番判断所有的结果
+      for (const result of results) {
+        result.then(r => {
+          completed++;
+          if (r)
+          //如果返回结果是true,因为这是or组合,所以也无需等待其他的返回结果,直接resolve大的Promise
+            resolve(true);
+          else if (completed === results.length)
+          //如果返回结果是false,且自己是最后一个了,直接返回false
+            resolve(false);
+        }).catch(err => {
+          completed++;
+          //如果有错误,无需等待其他,直接可以reject大的Promise
+          reject(err);
+        });
       }
-    });
-    const authResults = await Promise.all(promises);
-    for (const result of authResults) {
-      if (result) {
-        return true
-      }
-    }
-    return false;
-  };
+    })
+  }
 }
 
+//产生一个新的验证函数,将原验证函数返回值做and运算
 export function and(...authFuncs: AuthFunction[]): AuthFunction {
-  return async function (ctx: Context) {
-    //并行地执行验证函数
-    const promises = authFuncs.map(func => {
-      const promise = func(ctx) as any;
-      if (promise.then) {
-        return promise;
-      } else {
-        //如果不是一个promise,将结果转换为一个promise
-        return Promise.resolve(promise)
+  return function (ctx: Context) {
+    if (authFuncs.length < 1)
+      return true;
+
+    return new Promise((resolve, reject) => {
+      //并行地执行验证函数,并拿到结果
+      const results = authFuncs.map(func => {
+        const result = func(ctx) as any;
+        //将返回值promise化
+        return result.then ? result : Promise.resolve(result);
+      });
+
+      //已经完成的promise计数
+      let completed = 0;
+      //轮番判断所有的结果
+      for (const result of results) {
+        result.then(r => {
+          completed++;
+          //如果返回结果是false,因为这是and组合,所以也无需等待其他的返回结果,直接resolve大的Promise
+          if (!r)
+            resolve(false);
+          else if (completed === results.length)
+          //如果返回结果是true,且自己是最后一个了,直接返回true
+            resolve(true);
+        }).catch(err => {
+          completed++;
+          //如果有错误,无需等待其他,直接可以reject大的Promise
+          reject(err);
+        });
       }
-    });
-    const authResults = await Promise.all(promises);
-    for (const result of authResults) {
-      if (!result) {
-        return false
-      }
-    }
-    return true;
-  };
+    })
+  }
 }

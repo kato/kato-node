5040add5a9c97e7c00aaa03ecc4bd5371125e206
李雨田 <demon@danwi.me>
2019-03-18 14:19:13 +0800

    修正时间格式化时的正则表达式

--- core/common/json.ts
+++ core/common/json.ts
@@ -1,6 +1,6 @@
 import * as dayjs from 'dayjs';
 
-const ISO8601Regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]?\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]?\d:[0-5]\d|Z))/
+const ISO8601Regex = /^(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]?\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]?\d:[0-5]\d|Z))$/;
 
 export function reviver(key, value) {
   if (typeof value === 'string')
